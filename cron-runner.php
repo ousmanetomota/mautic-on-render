dab0f364c2d4f5263200e8ea9e5bf302da6fc73f
Ousmane Tomota <ousmanetomota@gmail.com>
2025-06-17 23:15:23 +0000

    Update cron-runner.php

--- cron-runner.php
+++ cron-runner.php
@@ -1,11 +1,9 @@
 <?php
-// Sécurité : clé secrète
 if (!isset($_GET['key']) || $_GET['key'] !== 'okacom2025') {
     http_response_code(403);
     die("Access denied.");
 }
 
-// Lancer les commandes cron de Mautic
 passthru("php /var/www/html/bin/console mautic:segments:update");
 passthru("php /var/www/html/bin/console mautic:campaigns:trigger");
 passthru("php /var/www/html/bin/console mautic:emails:send");
